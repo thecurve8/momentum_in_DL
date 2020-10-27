443269155053f028ca1009fdc68a5fc1fe371eee
Alexander Apostolov <alex21ander08@gmail.com>
2020-10-27 14:20:47 +0100

    dummy

--- src/crossvalidation.py
+++ src/crossvalidation.py
@@ -5,8 +5,7 @@ Created on Tue Oct 27 12:52:54 2020
 @author: Alexander
 """
 import numpy as np
-from torch.utils.data.sampler import Subset
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 import torch.optim as optim
 from train_optimizer import train_loop_optimizer
 
@@ -50,17 +49,48 @@ def create_train_val_dataloader(k, k_indices, dataset, args):
 def cross_validation_adam(model, learning_rates, dataset, K, criterion, args):
     
     k_indices = build_k_indices(len(dataset), K, args['seed'])
-    final_training_errors = []
-    final_validation_errors = []
-    training_errors_during_training = []
-    validation_errors_during_training = []
     
-    for lr in learning_rates:
-        for k in enumerate(K):
+    len_learning_rate = len(learning_rates)
+
+    training_errors_during_training = np.zeros((K, len_learning_rate, args['epochs']))
+    validation_errors_during_training = np.zeros((K, len_learning_rate, int(args['epochs']//args['log_interval']) ))
+    
+    for i, lr in enumerate(learning_rates):
+        for j, k in enumerate(range(K)):
             trainloader, validationloader = create_train_val_dataloader(k, k_indices, dataset, args)
-            
             optimizer = optim.Adam(model.parameters(), lr=lr)
-            train_loop_optimizer(model, trainloader, validationloader, optimizer, criterion, args['epochs'], args['log_interval'], args['cuda'])
+
+            train_losses, val_losses, train_accuracies, val_accuracies, _ =\
+                train_loop_optimizer(model, trainloader, validationloader,
+                                     optimizer, criterion, args['epochs'],
+                                     args['log_interval'], args['cuda'])
+            training_errors_during_training[i,j]=train_losses
+            validation_errors_during_training[i,j]=val_losses
+            
+    return training_errors_during_training, validation_errors_during_training
+
+def cross_validation_sgd(model, learning_rates, dataset, K, criterion, args):
+    
+    k_indices = build_k_indices(len(dataset), K, args['seed'])
+    
+    len_learning_rate = len(learning_rates)
+
+    training_errors_during_training = np.zeros((K, len_learning_rate, args['epochs']))
+    validation_errors_during_training = np.zeros((K, len_learning_rate, int(args['epochs']//args['log_interval']) ))
+    
+    for i, lr in enumerate(learning_rates):
+        for j, k in enumerate(range(K)):
+            trainloader, validationloader = create_train_val_dataloader(k, k_indices, dataset, args)
+            optimizer = optim.Adam(model.parameters(), lr=lr)
+
+            train_losses, val_losses, train_accuracies, val_accuracies, _ =\
+                train_loop_optimizer(model, trainloader, validationloader,
+                                     optimizer, criterion, args['epochs'],
+                                     args['log_interval'], args['cuda'])
+            training_errors_during_training[i,j]=train_losses
+            validation_errors_during_training[i,j]=val_losses
+            
+    return training_errors_during_training, validation_errors_during_training           
         
         
     
