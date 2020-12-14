f804b9f6a6f2a6b448e75759d31c4dcde4016d64
Alexander Apostolov <alex21ander08@gmail.com>
2020-12-14 23:47:32 +0100

    Remove batchnorm option

--- src/networks.py
+++ src/networks.py
@@ -158,8 +158,10 @@ def conv3x3(in_planes, out_planes, stride=1):
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, use_batchnorm=True):
         super(BasicBlock, self).__init__()
+        self.use_batchnorm = use_batchnorm
+        
         self.conv1 = conv3x3(in_planes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = conv3x3(planes, planes)
@@ -167,16 +169,27 @@ class BasicBlock(nn.Module):
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
+            if self.use_batchnorm:
+                self.shortcut = nn.Sequential(
+                    nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                    nn.BatchNorm2d(self.expansion*planes)
+                )
+            else:
+                self.shortcut = nn.Sequential(
+                    nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
+                )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
+        if self.use_batchnorm:
+            out = F.relu(self.bn1(self.conv1(x)))
+            out = self.bn2(self.conv2(out))
+            out += self.shortcut(x)
+            out = F.relu(out)
+        else:
+            out = F.relu(self.conv1(x))
+            out = self.conv2(out)
+            out += self.shortcut(x)
+            out = F.relu(out)
         return out
 
 
@@ -209,28 +222,33 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=10, use_batchnorm = True):
         super(ResNet, self).__init__()
         self.in_planes = 64
-
+        self.use_batchnorm = use_batchnorm
+        
         self.conv1 = conv3x3(3,64)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        if self.use_batchnorm:
+            self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1, use_batchnorm=self.use_batchnorm)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2, use_batchnorm=self.use_batchnorm)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2, use_batchnorm=self.use_batchnorm)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2, use_batchnorm=self.use_batchnorm)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layer(self, block, planes, num_blocks, stride, use_batchnorm):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, stride, use_batchnorm))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        if self.use_batchnorm:
+            out = F.relu(self.bn1(self.conv1(x)))
+        else:
+            out = F.relu(self.conv1(x))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
@@ -241,20 +259,20 @@ class ResNet(nn.Module):
         return out
 
 
-def ResNet18(num_classes=10):
-    return ResNet(BasicBlock, [2,2,2,2], num_classes)
+def ResNet18(num_classes=10, use_batchnorm=True):
+    return ResNet(BasicBlock, [2,2,2,2], num_classes, use_batchnorm)
 
-def ResNet34(num_classes=10):
-    return ResNet(BasicBlock, [3,4,6,3], num_classes)
+def ResNet34(num_classes=10, use_batchnorm=True):
+    return ResNet(BasicBlock, [3,4,6,3], num_classes, use_batchnorm)
 
-def ResNet50(num_classes=10):
-    return ResNet(Bottleneck, [3,4,6,3], num_classes)
+def ResNet50(num_classes=10, use_batchnorm=True):
+    return ResNet(Bottleneck, [3,4,6,3], num_classes, use_batchnorm)
 
-def ResNet101(num_classes=10):
-    return ResNet(Bottleneck, [3,4,23,3], num_classes)
+def ResNet101(num_classes=10, use_batchnorm=True):
+    return ResNet(Bottleneck, [3,4,23,3], num_classes, use_batchnorm)
 
-def ResNet152(num_classes=10):
-    return ResNet(Bottleneck, [3,8,36,3], num_classes)
+def ResNet152(num_classes=10, use_batchnorm=True):
+    return ResNet(Bottleneck, [3,8,36,3], num_classes, use_batchnorm)
 
 # def test_resnet():
 #     net = ResNet50()
